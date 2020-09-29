f9a6c3001c36c14de06fa5da39b5a615f33c5d45
zongzhibin9 <zongzhibin9@163.com>
2020-09-29 11:27:07 +0800

    增加openfeign  以及API模块提取

--- zxkj-cloud/zxkj-modules/zxkj-consumer/src/main/java/com/zxkj/consumer/ConsumerApplication.java
+++ zxkj-cloud/zxkj-modules/zxkj-consumer/src/main/java/com/zxkj/consumer/ConsumerApplication.java
@@ -1,6 +1,5 @@
 package com.zxkj.consumer;
 
-import com.zxkj.consumer.annotations.EnableRyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
