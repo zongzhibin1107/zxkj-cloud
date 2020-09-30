63e1952141806f494c1f636a968c926a0ceab665
zongzhibin9 <zongzhibin9@163.com>
2020-09-30 08:26:23 +0800

    修改 consumer模块支持使用nacos配置

--- zxkj-cloud/zxkj-modules/zxkj-consumer/src/main/java/com/zxkj/consumer/controller/ConsumerController.java
+++ zxkj-cloud/zxkj-modules/zxkj-consumer/src/main/java/com/zxkj/consumer/controller/ConsumerController.java
@@ -4,6 +4,7 @@ import com.zxkj.common.core.domain.R;
 import com.zxkj.system.api.RemoteSystemTestService;
 import com.zxkj.system.api.domain.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,18 @@ public class ConsumerController {
     @Autowired
     RemoteSystemTestService remoteSystemTestService;
 
+    @Value("${zxkj.test}")
+    private String value;
+
 
     @GetMapping("/consumer/test")
     public R<Test> test(){
         return remoteSystemTestService.test();
+    }
+
+
+    @GetMapping("/consumer/testget")
+    public R<String> testget(){
+        return R.ok(value + "-" );
     }
 }
