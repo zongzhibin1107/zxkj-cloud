f29b76540cd36da16a973002bf7bd85bdca197bd
zongzhibin9 <zongzhibin9@163.com>
2020-09-30 08:45:02 +0800

    网关模块基本功能添加

---
+++ zxkj-cloud/zxkj-gate-server/src/main/java/com/zxkj/gateway/ZxkjGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.zxkj.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication
+public class ZxkjGatewayApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ZxkjGatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依网关启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
